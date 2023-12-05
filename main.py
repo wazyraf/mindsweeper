23beb49b59501dc741f82abc22225c74d89ff351
Erwin <erwinmario10@gmail.com>
2023-12-05 16:46:46 +0200

    mainpage directory

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # main.py
 import tkinter as tk
-import play_page
-import settings_page
-import account_page
+from mainpages import play_page
+from mainpages import settings_page
+from mainpages import account_page
 
 class MainPage(tk.Frame):
     def __init__(self, master=None):
@@ -11,6 +11,13 @@ class MainPage(tk.Frame):
         self.master.title("Main Page")
         self.pack()
 
+        #get screen width and height - for windowed full-screen
+        #screen_width = self.master.winfo_screenwidth()
+        #creen_height = self.master.winfo_screenheight()
+
+        #set window size
+        self.master.geometry(f'{800}x{600}')
+        
         play_button = tk.Button(self, text="Play", command=lambda: self.show_frame(play_page.PlayPage))
         settings_button = tk.Button(self, text="Settings", command=lambda: self.show_frame(settings_page.SettingsPage))
         account_button = tk.Button(self, text="Account", command=lambda: self.show_frame(account_page.AccountPage))
