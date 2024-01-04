616763be75146e69fee734eae8bed4fa5febdfa9
GabrielFronea <gabrielfronea2003@gmail.com>
2024-01-04 13:37:29 +0200

    butoane main page

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import flet as ft
 from flet import View, Page, AppBar, ElevatedButton, Text
 from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAlignment
 from mainpages.play_page import play_page_view
+from mainpages.settings_page import settings_page_view
+from mainpages.account_page import account_page_view
 
 def main(page: Page) -> None:
     page.title = "MAIN PAGE"
@@ -16,7 +18,9 @@ def main(page: Page) -> None:
                 controls=[
                     AppBar(title= Text('HOME'), bgcolor='blue'),
                     Text(value='HOME', size=30),
-                    ElevatedButton(text='Play Page', on_click=lambda _: page.go('/play_page'))
+                    ElevatedButton(text='Play', on_click=lambda _: page.go('/play_page')),
+                    ElevatedButton(text='Account', on_click=lambda _: page.go('/account_page')),
+                    ElevatedButton(text='Settings', on_click=lambda _: page.go('/settings_page'))
                     ],
                     vertical_alignment=MainAxisAlignment.CENTER,
                     horizontal_alignment=CrossAxisAlignment.CENTER,
@@ -27,6 +31,14 @@ def main(page: Page) -> None:
         #play_page
         if page.route == '/play_page':
             page.views.append(play_page_view(page))
+
+        #settings_page
+        if page.route == '/settings_page':
+            page.views.append(settings_page_view(page))
+
+        #account_page
+        if page.route == '/account_page':
+            page.views.append(account_page_view(page))
     page.update()
 
     def view_pop(e: ViewPopEvent) -> None:
