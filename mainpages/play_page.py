e2b9ed0963754196ee7afed0fa285f2c14a92e07
GabrielFronea <gabrielfronea2003@gmail.com>
2024-01-08 13:10:01 +0200

    Play page(matrice colorata random)

--- mainpages/play_page.py
+++ mainpages/play_page.py
@@ -2,11 +2,14 @@
 from flet import View, Page, AppBar, ElevatedButton, Text
 from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAlignment
 from flet import Column, UserControl, Row, Container, border
+import random
 
 class GenerateGrid(UserControl):
-    def __init__(self):
+    def __init__(self, difficulty):
         #variabilele mai tarziu
         self.grid = Column(opacity=1, animate_opacity=300)
+        self.blue_tiles: int = 0
+        self.difficulty: int = difficulty
         super().__init__()
     def build(self):
         rows: list=[
@@ -25,10 +28,23 @@ class GenerateGrid(UserControl):
             )
             for _ in range(5)
         ]
+
+
+        colors: list = ["#5c443b", "#4cbbb5"]
+
+        for row in rows: 
+            for container in row.controls[:]:
+                container.bgcolor = random.choices(colors, weights = [10, self.difficulty])[0]
+                container.data = container.bgcolor
+                if container.bgcolor == "#4cbbb5":
+                    self.blue_tiles += 1
+ 
         self.grid.controls = rows
         return self.grid
 
         pass
+
+    
 
 def play_page_view(page: Page):
     return View(
@@ -36,8 +52,8 @@ def play_page_view(page: Page):
         controls=[
             AppBar(title= Text('PLAY PAGE'), bgcolor='blue'),
             Text(value='PLAY PAGE', size=30),
-            GenerateGrid(),
-            ElevatedButton(text='Go Back', on_click=lambda _: page.go('/'))
+            GenerateGrid(2),
+            ElevatedButton(text='Start', on_click=lambda _: page.go('/')),
             ],
             vertical_alignment=MainAxisAlignment.CENTER,
             horizontal_alignment=CrossAxisAlignment.CENTER,
