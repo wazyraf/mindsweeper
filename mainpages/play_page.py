d6f404acc1e3bb1c8cc24cd5686a845cf27d992b
Erwin <erwinmario10@gmail.com>
2024-01-08 12:15:44 +0200

    Matrice in main

--- mainpages/play_page.py
+++ mainpages/play_page.py
@@ -1,7 +1,34 @@
 #play_page.py
 from flet import View, Page, AppBar, ElevatedButton, Text
 from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAlignment
+from flet import Column, UserControl, Row, Container, border
 
+class GenerateGrid(UserControl):
+    def __init__(self):
+        #variabilele mai tarziu
+        self.grid = Column(opacity=1, animate_opacity=300)
+        super().__init__()
+    def build(self):
+        rows: list=[
+            Row(
+                alignment=MainAxisAlignment.CENTER,
+                controls=[
+                    Container(
+                        width=54,
+                        height=54,
+                        animate=300,
+                        border=border.all(1,'white'),
+                        on_click= None #schimbam imediat
+                    )
+                    for _ in range(5)
+                ],
+            )
+            for _ in range(5)
+        ]
+        self.grid.controls = rows
+        return self.grid
+
+        pass
 
 def play_page_view(page: Page):
     return View(
@@ -9,6 +36,7 @@ def play_page_view(page: Page):
         controls=[
             AppBar(title= Text('PLAY PAGE'), bgcolor='blue'),
             Text(value='PLAY PAGE', size=30),
+            GenerateGrid(),
             ElevatedButton(text='Go Back', on_click=lambda _: page.go('/'))
             ],
             vertical_alignment=MainAxisAlignment.CENTER,
