72534da166f413a1f584bb00112e6fa11451006f
GabrielFronea <gabrielfronea2003@gmail.com>
2024-01-10 22:42:32 +0200

    settings_page

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
     •Actualizarea stage-ului dupa fiecare nivel: https://chat.openai.com/share/b2a737dd-3f6d-4cfb-b006-ab8a7b3b9cd3
 
     •flet run main.py -d --> pentru rulare cu hot reload (ajuta in modificarea elementelor UI, fara a porni si opri aplicatia), documentatie: https://flet.dev/docs/guides/python/hot-reload/
+
+    •pentru DropDown --> https://flet.dev/docs/controls/dropdown/
---
+++ mainpages/settings_page.py
@@ -0,0 +1,61 @@
+# play_page.py
+import flet as ft
+from flet import View, Page, AppBar, Text, IconButton, Row, Column, Switch, ElevatedButton, Container
+from flet import CrossAxisAlignment, MainAxisAlignment
+
+def settings_page_view(page: Page):
+    def dropdown_changed(e):
+        selected_value = e.new_value
+        print(f"Dropdown changed to {selected_value}")
+        page.update()
+
+    return View(
+        route='/settings_page',
+        controls=[
+            AppBar(title=Text('SETTINGS PAGE'), bgcolor='blue',
+                   actions=[
+                       Row([
+                           IconButton(
+                               icon=ft.icons.SETTINGS,
+                               icon_color='white',
+                               on_click=lambda _: page.go('/settings_page')
+                           ),
+                           IconButton(
+                               icon=ft.icons.ACCOUNT_CIRCLE,
+                               icon_color='white',
+                               on_click=lambda _: page.go('/profile_page')
+                           )
+                       ]),
+                   ]),    
+            Row([
+                Text(value="Sound", color='blue', size= 20),
+                Switch(
+                    value=True,
+                    on_change=lambda val: print(f'Sound state: {val}')
+                )
+            ],
+            alignment=MainAxisAlignment.CENTER
+            ),
+            Container(
+                height= 20
+            ),
+            Row([
+                Text(value="Theme", color='blue', size=20),
+                ft.Text(),
+                ft.Dropdown(
+                    on_change = dropdown_changed,
+                    options=[
+                        ft.dropdown.Option("Default Theme"),
+                        ft.dropdown.Option("Light Theme"),
+                        ft.dropdown.Option("Dark Theme"),
+                    ],
+                    width=200,
+                    value="Default Theme"
+                )
+            ],
+            alignment=MainAxisAlignment.CENTER)  
+        ],
+        vertical_alignment=MainAxisAlignment.CENTER,
+        horizontal_alignment=CrossAxisAlignment.CENTER,
+        spacing=20
+    )
